6f9ae2bfcd110fb207dadb05199b93e80f6b7421
Ayesha Fahim <ayesha.fahim02@gmail.com>
2025-05-21 18:17:01 +0500

    ***

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import { View, Text } from 'react-native';
 import Splash from './src/Screens/Splash'; // Adjust path if needed
 import Onboarding from './src/Screens/Onboarding';
 import OnboardingTwo from './src/Screens/OnboardingTwo';
+import ForgotPass from './src/Screens/ForgotPass';
+import InboxOTP from './src/Screens/InboxOTP';
 
 export default function App() {
   return (
-    <OnboardingTwo />
+    <InboxOTP/>
   );
 }
